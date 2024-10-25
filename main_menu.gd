2613ce5f232c4f814c654ba3539550c68758ae6a
Kevin Lam <kevlam04@gmail.com>
2024-10-24 21:30:26 -0400

    Made menu
    Need to do options menu still. It is just blank.

--- main_menu.gd
+++ main_menu.gd
@@ -1,12 +1,21 @@
+class_name MainMenu
 extends Control
+
+@onready var new_game = $"VBoxContainer/New Game" as Button
+@onready var load_game = $"VBoxContainer/Load Game" as Button
+@onready var options = $VBoxContainer/Options as Button
+@onready var quit_game = $"VBoxContainer/Quit Game" as Button
+@onready var options_panel = $OptionsPanel as PanelContainer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
+	#new_game.pressed.connect() #Connect this to opening the menu to make a new game.
+	#load_game.pressed.connect() #Connect this open up a load game menu.
+	options.pressed.connect(options_menu) #Connect this to open up an options menu.
+	quit_game.pressed.connect(get_tree().quit)
 	pass
+
+
+func options_menu() -> void:
+	options_panel.visible = true
