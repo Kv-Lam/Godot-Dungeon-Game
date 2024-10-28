7810580f1b6dd565ff3f106097b9242164667893
Protoclasm <kevlam04@gmail.com>
2024-10-28 10:03:38 -0400

    Worked on main menu
    Added exit button and more containers along with text. Need to work on functionality.

--- main_menu.gd
+++ main_menu.gd
@@ -1,3 +1,4 @@
+#Kevin's
 class_name MainMenu
 extends Control
 
@@ -11,10 +12,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#new_game.pressed.connect() #Connect this to opening the menu to make a new game.
-	#load_game.pressed.connect() #Connect this open up a load game menu.
+	#load_game.pressed.connect() #Connect this to open up a load game menu.
 	options.pressed.connect(options_menu) #Connect this to open up an options menu.
 	quit_game.pressed.connect(get_tree().quit)
-	pass
 
 
 func options_menu() -> void:
---
+++ options_panel.gd
@@ -0,0 +1,15 @@
+#Kevin's
+extends PanelContainer
+
+@onready var options_panel: PanelContainer = $"."
+@onready var close_options: Button = $"MarginContainer/VBoxContainer/Close Options"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	close_options.pressed.connect(exit_options)
+	pass # Replace with function body.
+
+
+func exit_options() -> void:
+	options_panel.visible = false
