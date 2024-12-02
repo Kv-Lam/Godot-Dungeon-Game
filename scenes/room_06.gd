8a431d22df4dddf8d241b904f9e36b5829ec223a
Doctor-Johnson <gpstcob@gmail.com>
2024-12-02 00:32:51 -0500

    Rooms, Rooms, Rooms, and more Rooms

---
+++ scenes/room_06.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var player = scene_manager.player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if scene_manager.player:
+		add_child(scene_manager.player)
