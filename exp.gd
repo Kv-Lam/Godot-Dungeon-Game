ef2e515d36929f93c14630e0921393f68731b2dc
Minh Cao <minhcaooo34@gmail.com>
2024-12-02 16:37:03 -0500

    updated character

--- exp.gd
+++ exp.gd
@@ -3,9 +3,18 @@ class_name levelxp extends Node
 var xp: int = 0
 var points: int = 0
 
-func get_xp() -> int:
+func getXP() -> int:
 	return xp
 
+func levelUp(stat) -> int:
+	if stat >= 10:
+		print("Stat is maxed already")
+		return null
+	else:
+		stat++
+		return stat
+		
+		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
