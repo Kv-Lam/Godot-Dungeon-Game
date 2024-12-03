9927c5dce9d219a304f4b0da2fa96ce0afd9ce8a
Minh Cao <minhcaooo34@gmail.com>
2024-12-03 03:24:26 -0500

    finished up leveling up

--- Scenes/character_level.gd
+++ Scenes/character_level.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var node_stat_points = get_node("HBoxContainer/VBoxContainer/HBoxContainer/MainStats/HBoxContainer/Points")
-var path_main_stats = "HBoxContainer/VBoxContainer/HBoxContainer/MainStats"
+var path_main_stats = "./HBoxContainer/VBoxContainer/HBoxContainer/MainStats"
 
 var availablePoints = 5
 var healthAdd = 0 
@@ -10,7 +10,8 @@ var attackAdd = 0
 var defenseAdd = 0 
 var dodgeAdd = 0 
 
-func _ready() -> void:
+
+func _ready():
 	node_stat_points.set_text(str(availablePoints) + " Points")
 	if availablePoints == 0:
 		pass
@@ -18,14 +19,35 @@ func _ready() -> void:
 		for button in get_tree().get_nodes_in_group("PlusButtons"):
 			button.set_disabled(false)
 	for button in get_tree().get_nodes_in_group("PlusButtons"):
-		button.connect("pressed", self, "IncreaseStat", [button.get_node("../..").get_name()])
+		button.connect("pressed", Callable(IncreaseStat.bind(button.get_node("../..").get_name())))
 	for button in get_tree().get_nodes_in_group("MinButtons"):
-		button.connect("pressed", self, "DecreaseStat", [button.get_node("../..").get_name()])
+		button.connect("pressed", Callable(DecreaseStat.bind(button.get_node("../..").get_name())))
 		
 		
 func IncreaseStat(stat):
-	print(stat + "Plus")
-	
-	
+	set(stat.to_lower() + "Add", get(stat.to_lower() + "Add") + 10)
+	get_node(path_main_stats + "/" + stat + "/StatBackground/Stat/Change").set_text("+" + str(get(stat.to_lower() 
+																				+ "Add")) + " ")
+	#unlocking minus button
+	get_node(path_main_stats + "/" + stat + "/StatBackground/Minus").set_disabled(false)
+	#reduce available stat points
+	availablePoints -= 1
+	#update available stat point in lable
+	node_stat_points.set_text(str(availablePoints) + " Points")
+	#if available points is 0
+	if availablePoints == 0:
+		for button in get_tree().get_nodes_in_group("PlusButtons"):
+			button.set_disabled(true)
+
 func DecreaseStat(stat):
-	print(stat + "Minus")
+	set(stat.to_lower() + "Add", get(stat.to_lower() + "Add") - 10)
+	if get(stat.to_lower() + "Add") == 0:
+		get_node(path_main_stats + "/" + stat + "/StatBackground/Minus").set_disabled(true)
+		get_node(path_main_stats + "/" + stat + "/StatBackground/Stat/Change").set_text("")
+	else:
+		get_node(path_main_stats + "/" + stat + "/StatBackground/Stat/Change").set_text("+" + str(get(stat.to_lower()
+																				+ "Add")) + " ")
+	availablePoints += 1
+	node_stat_points.set_text(str(availablePoints) + " Points")
+	for button in get_tree().get_nodes_in_group("PlusButtons"):
+		button.set_disabled(false)
